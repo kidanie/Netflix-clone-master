ea10911c8a0d6f509686444ad332ba20ffa5d4e9
kidanie <kidaniek@gmail.com>
2021-03-17 00:00:39 -0700

    Initial commit

---
+++ Netflix-clone-master/build/precache-manifest.b6a50cfd5f3c1c391ae1b49cd9263864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac858ecee245d3dca6f454b3cd1edc51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e2b89c7072232247d60",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2b89c7072232247d60",
+    "url": "/static/js/main.79f90c95.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
